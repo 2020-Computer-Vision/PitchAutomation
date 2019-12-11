edff88bfe2a1a86cf42cff41ea3d1cdabd425a14
Jacob Morris <jacobm3@code.vt.edu>
2019-12-11 15:56:09 -0500

    Pitch automation working

--- load_video.py
+++ load_video.py
@@ -11,7 +11,8 @@ import pandas as pd
 import cv2
 import time
 
-video_name = "Duke Game 1 Center Field.mp4"
+#video_name = "UVA4-13-19Game2.mp4"
+video_name = "Duke Game 1 3rd Base Dugout.mp4"
 
 vidObj = cv2.VideoCapture(video_name)
 
@@ -19,15 +20,18 @@ count = 1;
 
 start = time.time()
 
-while count <= 150:
+frames = {217,513,915,989,1223,1528,1546,1581,1856,1885,2315,2769,
+            2793,2808,3123,3529,3547,3966,4250}
+
+while count <= 4300:
     
     # Image dimensions: (720, 1280)
     success, image = vidObj.read()
     #image = image[:,int(1280/4): int(1280*3/4)]
     
-    #if (count == 1):
+    if (count in frames):
         #image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    cv2.imwrite("frames/frame{:05d}.jpg".format(count), image)
+        cv2.imwrite("frames/frame{:05d}_pitchSIDE.jpg".format(count), image)
     
     count += 1;
     
